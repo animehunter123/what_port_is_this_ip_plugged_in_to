4533654304a62d0b72c86ef61eac9fd489bd274d
animehunter123 <42163211+animehunter123@users.noreply.github.com>
2024-12-19 15:09:05 +0900

    helper script for fresh os

---
+++ install-prereqs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Installing prereqs, npm"
+apt update
+apt install -y npm
+
+cd backend
+# npm i express cors node-pty #This is manual way, but i put everything in packages.json so...
+npm i
+cd ..
+
+cd frontend
+npm i
+cd ..
+
+echo "Prereqs installed"
